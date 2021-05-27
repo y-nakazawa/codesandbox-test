37fc85044273c9916d225fd6a3a8bfb09108dce2
Yuichi Nakazawa <y_nakazawa@jsl.co.jp>
2021-05-27 22:23:20 +0900

    一旦commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-import "./styles.css";
+// const name = "katekichi";
+// const age = 43;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const msg1 = "私の名前は" + name;
+// console.log(msg1);
+
+// const msg2 = `私の名前は${name}です`;
+
+// console.log(msg2);
+
+const func2 = (str) => str;
+
+console.log(func2("This is func2"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 20));
+
+// const myProfile = {
+//   name: "katekichi",
+//   age: 43
+// };
+
+// const { name, age } = myProfile;
+// const msg3 = `名前は${name}です。年齢は${age}です`;
+// console.log(msg3);
+
+const myProfile = ["katekichi", 43];
+const [name, age] = myProfile;
+const msg3 = `名前は${name}です。年齢は${age}です`;
+console.log(msg3);
+
+const sayHello = (name = "Guest") => console.log(`名前は${name}です`);
+sayHello("katekichi");
+
+const ary1 = [1, 2];
+console.log(...ary1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(...ary1);
+
+// まとめる
+const ary2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...ary3] = ary2;
+console.log(num1);
+console.log(num2);
+console.log(ary3);
+
+// 配列のコピー
+const ary4 = [10, 20];
+const ary5 = [30, 40];
+
+const ary6 = [...ary4];
+console.log(ary6);
+
+const ary7 = [...ary4, ...ary5];
+console.log(ary7);
+
+// mapとfilter
+const nameAry = ["Tanaka", "Yamada", "katekichi"];
